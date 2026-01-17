ba9079c2d6a8d619c7ca3964a9b8641e9ba09bf4
vasu <59436288+i-vasu@users.noreply.github.com>
2026-01-17 10:33:14 +0530

    Revert "Fix build and runtime errors in order-service and product-service"

--- order-service/src/main/java/com/app/repositories/CartRepo.java
+++ order-service/src/main/java/com/app/repositories/CartRepo.java
@@ -14,6 +14,6 @@ public interface CartRepo extends JpaRepository<Cart, Long> {
 	@Query("SELECT c FROM Cart c WHERE c.user.email = ?1 AND c.id = ?2")
 	Cart findCartByEmailAndCartId(String email, Long cartId);
 
-	@Query("SELECT c FROM Cart c JOIN FETCH c.cartItems ci WHERE ci.productId = ?1")
+	@Query("SELECT c FROM Cart c JOIN FETCH c.cartItems ci JOIN FETCH ci.product p WHERE p.id = ?1")
 	List<Cart> findCartsByProductId(Long productId);
 }
